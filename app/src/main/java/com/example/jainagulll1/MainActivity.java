6bd28835a48ab803256922504b410deb6414ad43
babaevajainagul <babaevajainagul13@gmail.com>
2024-10-08 11:34:21 +0600

    Initial commit

---
+++ app/src/main/java/com/example/jainagulll1/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.jainagulll1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edText;
+    Button button;
+    TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edText = findViewById(R.id.editText);
+        button = findViewById(R.id.btn);
+        text= findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt = edText.getText().toString();
+                text.setText(txt);
+
+            }
+        });
+
+
+    }
+}
